5e6e180e08bfc2b2c48418d6333a0584d945b7e9
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-08-04 22:40:43 -0300

    adicionando handlers de próximo e anterior

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,28 @@ function App() {
     'Testes automatizados',
   ];
 
+  const [carrousselIndex, setCarrousselIndex] = useState(0);
+  const [newTool, setNewTool] = useState('');
+  const [toolKitLi, setToolKitLi] = useState(toolKit);
+
+  function handleCarrousselNext() {
+    carrousselIndex + 1 < toolKitLi.length ?
+      setCarrousselIndex(carrousselIndex + 1) : setCarrousselIndex(0)
+  }
+
+  function handleCarrousselPrevious() {
+    carrousselIndex - 1 < 0 ?
+      setCarrousselIndex(toolKitLi.length - 1) : setCarrousselIndex(carrousselIndex - 1)
+  }
+
+
   return (
     <>
       <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
-      <h2></h2>
+      <h2>{toolKit[carrousselIndex]}</h2>
       <div className='btn-container'>
-        <button>Anterior</button>
-        <button>Próximo</button>
+        <button onClick={handleCarrousselPrevious}>Anterior</button>
+        <button onClick={handleCarrousselNext}>Próximo</button>
       </div>
       <section>
         <h3>Adicione novas ferramentas:</h3>
@@ -27,7 +42,11 @@ function App() {
       </section>
       <section>
         <h3>Lista de intens do carrossel:</h3>
-        <ul></ul>
+        <ul>
+          {
+            toolKitLi.map((e, i) => <li key={i}>{ e }</li>)
+          }
+        </ul>
       </section>
     </>
   )
